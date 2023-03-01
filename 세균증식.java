68f88b14cee4146fb221ab236194edf55bd013cd
ColdBrew <99588377+Quokka2@users.noreply.github.com>
2023-03-01 10:57:11 +0900

    Add files via upload

---
+++ 세균증식.java
@@ -0,0 +1,12 @@
+package programmers;
+
+public class 세균증식 {
+
+	public static void main(String[] args) {
+
+		double n = 7;
+		double t = 15;
+		double a = n * Math.pow(2, t);
+		System.out.println(a);
+	}
+}
